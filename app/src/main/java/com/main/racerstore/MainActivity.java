853393f3b0680bc51b2e9535a7406280a5f053ba
Alex <geminis.alex@hotmail.com>
2023-06-06 14:47:37 -0500

    06-06-23 Implementación correcta del login con XAMP y mariaDB base de datos racerstore

--- app/src/main/java/com/main/racerstore/MainActivity.java
+++ app/src/main/java/com/main/racerstore/MainActivity.java
@@ -4,26 +4,116 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
+    private EditText etUser;
+    private EditText etPass;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnLogin = findViewById(R.id.login);
+        etUser = findViewById(R.id.log);
+        etPass = findViewById(R.id.pass);
+        btnLogin = findViewById(R.id.login);
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Crear un Intent para abrir la actividad de inicio de sesión
-                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
-                startActivity(intent);
+                String user = etUser.getText().toString().trim();
+                String pass = etPass.getText().toString().trim();
+
+                // Realizar la solicitud HTTP al archivo PHP
+                login(user, pass);
+            }
+        });
+    }
+
+
+    private void login(String user, String pass) {
+        OkHttpClient client = new OkHttpClient();
+
+        // Construir el cuerpo de la solicitud con los parámetros de usuario y contraseña
+        RequestBody requestBody = new FormBody.Builder()
+                .add("user", user)
+                .add("pass", pass)
+                .build();
+
+        // Construir la solicitud POST
+        Request request = new Request.Builder()
+                .url("http://192.168.1.45/RacerStore/login.php")  // Reemplaza con la URL de tu archivo PHP en el servidor
+                .post(requestBody)
+                .build();
+
+        // Enviar la solicitud de forma asíncrona
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                // Manejar la respuesta del servidor
+                if (response.isSuccessful()) {
+                    // La solicitud fue exitosa
+                    String responseData = response.body().string();
+                    // Aquí puedes manejar la respuestas del servidor según lo que devuelva tu archivo PHP
+                    if (responseData.equals("Inicio de sesion exitosa")) {
+                        // El inicio de sesión fue exitoso
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(MainActivity.this, "Inicio de sesion exitosa", Toast.LENGTH_SHORT).show();
+                                // Aquí puedes redirigir a otra actividad o realizar alguna acción adicional
+                                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                                startActivity(intent);
+                            }
+                        });
+                    } else {
+                        // Las credenciales son inválidas
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(MainActivity.this, "Credenciales invalidas", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                } else {
+                    // La solicitud falló
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "Error en la solicitud", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                // Error de conexión
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "Error de conexión", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
     }
 }
+
+
--- app/src/main/java/com/main/racerstore/SearchActivity.java
+++ app/src/main/java/com/main/racerstore/SearchActivity.java
@@ -1,31 +1,23 @@
 package com.main.racerstore;
 
 import androidx.appcompat.app.AppCompatActivity;
-import java.sql.Connection;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-
 public class SearchActivity extends AppCompatActivity {
 
-    private DBHelper dbHelper;
-    private Connection connection;
     private EditText tbuscar;
-    private Button search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        dbHelper = new DBHelper();
-        connection = dbHelper.getConnection();
-
         tbuscar = findViewById(R.id.tbuscar);
-        search = findViewById(R.id.search);
 
         Button btnSearch = findViewById(R.id.search);
         btnSearch.setOnClickListener(new View.OnClickListener() {
@@ -38,14 +30,9 @@ public class SearchActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        dbHelper.closeConnection();
-    }
-
     private void searchProducts(String tbuscar) {
-        // Aquí puedes realizar la búsqueda de productos utilizando la conexión y el texto de búsqueda
-        // Recuerda manejar adecuadamente las excepciones y cerrar los recursos apropiadamente
+        // Aquí puedes realizar la búsqueda de productos utilizando PHP como puente de conexión
+        // Recuerda manejar adecuadamente las respuestas y procesar los resultados obtenidos
     }
 }
+
