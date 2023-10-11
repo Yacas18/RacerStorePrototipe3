622a930978cb3b8f3812aaae447598dc8595a32a
Alex <geminis.alex@hotmail.com>
2023-10-11 17:02:40 -0500

    16:36pm 11-10-23
    Prototipo v4

--- app/src/main/java/com/main/racerstore/SearchActivity.java
+++ app/src/main/java/com/main/racerstore/SearchActivity.java
@@ -5,16 +5,25 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SearchActivity extends AppCompatActivity {
+    private HorizontalScrollView horizontalScrollView;
+    private int scrollX = 0;
+
+    private final Handler handler = new Handler();
+    private final int scrollDelay = 5; // Tiempo en milisegundos entre desplazamientos
+    private final int scrollSpeed = 2; // Velocidad del desplazamiento
+    private boolean scrollRight = true;
     private boolean isButtonEnabled = true;
     private EditText tbuscar;
     private SearchController searchController;
@@ -61,6 +70,18 @@ public class SearchActivity extends AppCompatActivity {
                 return true;
             }
         });
+        horizontalScrollView = findViewById(R.id.horizontalScrollView2);
+
+        // Agregar un escuchador para detectar cambios en el desplazamiento
+        horizontalScrollView.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
+            @Override
+            public void onScrollChanged() {
+                scrollX = horizontalScrollView.getScrollX();
+            }
+        });
+
+        // Iniciar el desplazamiento automático
+        startAutoScroll();
     }
 
     private void performSearch() {
@@ -96,5 +117,19 @@ public class SearchActivity extends AppCompatActivity {
             tiempoUltimaPulsacion = tiempoActual;
             Toast.makeText(this, "Presiona nuevamente para salir", Toast.LENGTH_SHORT).show();
         }
+    }
+    private void startAutoScroll() {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                scrollX += scrollSpeed; // Aumentar la posición de desplazamiento
+                if (scrollX >= horizontalScrollView.getChildAt(0).getMeasuredWidth() - horizontalScrollView.getMeasuredWidth()) {
+                    // Has llegado al final, así que puedes volver al principio sin pausa
+                    scrollX = 0;
+                }
+                horizontalScrollView.scrollTo(scrollX, 0); // Realizar el desplazamiento
+                handler.postDelayed(this, scrollDelay); // Programar el próximo desplazamiento
+            }
+        }, scrollDelay);
     }
 }
